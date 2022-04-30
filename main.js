8c4ea378e61dfd4dba177255826d3eb16f5b822d
Ola Kida <oladra6@gmail.com>
2022-04-30 16:38:44 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,82 @@
+// animations
+
+gsap.to("#Head-1", {
+    duration: 1.01,
+    x: 8,
+    y:3,
+    repeat: -1,
+    yoyo: true
+  });
+  //var Tilt = document.getElementById("Head-2");
+  gsap.to("#Hands-1", {
+    duration: 0.3,
+    y: -5.5,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Hands-3", {
+    duration: 0.3,
+    y: 5.5,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Flower-1", {
+    duration: 0.8,
+    x: 6,
+    y:-2,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Flower-2", {
+    duration: 0.5,
+    x: -4,
+    y:2,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Flower-3", {
+    duration: 0.6,
+    x: 2,
+    y:-1,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Leg-1", {
+    duration: 1.82,
+    y: -5,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Leg-3", {
+    duration: 1,
+    x: -5,
+    y: 2,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.from("#Head-2", {
+    rotation:4,
+    duration:0.812,
+    repeat: -1,
+    yoyo:true
+  });
+  gsap.to("#Hands-2", {
+    duration: 0.89,
+    y: 5.5,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Tablet", {
+    duration: 0.89,
+    y: 4,
+    x: -2,
+    repeat: -1,
+    yoyo: true
+  });
+  gsap.to("#Leg-2", {
+    duration: 0.7,
+    y: 3,
+    x: -2.8,
+    repeat: -1,
+    yoyo: true
+  });
